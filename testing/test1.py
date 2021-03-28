9c110ea39edb99b3ad1d0e456ac197550d36b379
codingsona <sonali.github@gmail.com>
2021-03-27 23:39:25 -0700

    testing started..

---
+++ testing/test1.py
@@ -0,0 +1,29 @@
+import unittest
+
+def add(a, b):
+    return a+b
+
+def subtract(a, b):
+    return a-b
+
+boolean = True
+
+
+class TestAdd(unittest.TestCase):
+    def test_add(self):
+        #self.assertEqual(2, 2)
+        self.assertEqual(add(2, 4), 6)
+        self.assertNotEqual(add(2,4), 10)
+
+    def test_bool(self):
+        self.assertTrue(boolean)
+        #self.assertFalse(boolean)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(30, 10), 20)
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
---
+++ testing/test2.py
@@ -0,0 +1,31 @@
+import unittest
+import testing.test1
+
+
+class TestAdd(unittest.TestCase):
+    def setUp(self):
+        self.number = None
+        self.boolean = True
+        self.a = 5
+        self.b = 6
+
+    def test_add(self):
+        #self.assertEqual(2, 2)
+        self.assertEqual(testing.test1.add(self.a, self.b), 11)
+        self.assertNotEqual(testing.test1.add(2,4), 10)
+
+    def test_bool(self):
+        self.assertTrue(testing.test1.boolean)
+        #self.assertFalse(boolean)
+
+    def test_subtract(self):
+        self.assertEqual(testing.test1.subtract(30, 10), 20)
+
+    def tearDown(self):
+        print(self.boolean)
+        del self.boolean
+
+
+
+if __name__ == "__main__":
+    unittest.main()
