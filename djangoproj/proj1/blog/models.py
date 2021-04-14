e1b1adb9ca2adaea689987333bd0e7115ff39b49
codingsona <sonali.github@gmail.com>
2021-04-13 20:55:43 -0700

    add urls to the view

--- djangoproj/proj1/blog/models.py
+++ djangoproj/proj1/blog/models.py
@@ -24,3 +24,21 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+    
+from django.urls import reverse
+class Post(models.Model):
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()# Our custom manager.
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day, self.slug])
+
+
+
+
+
